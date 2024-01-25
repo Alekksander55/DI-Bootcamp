ae7c21b70a7eea18c446a2bfd43f1b00a3542e0c
Alekksander55 <alexandertemim@gmail.com>
2024-01-25 14:20:34 +0200

    modified:   Week 10/Day 2/dailychallenge/client/src/App.js 	modified:   Week 10/Day 2/dailychallenge/node_modules/.package-lock.json 	new file:   Week 10/Day 2/dailychallenge/node_modules/cors/CONTRIBUTING.md 	new file:   Week 10/Day 2/dailychallenge/node_modules/cors/HISTORY.md 	new file:   Week 10/Day 2/dailychallenge/node_modules/cors/LICENSE 	new file:   Week 10/Day 2/dailychallenge/node_modules/cors/README.md 	new file:   Week 10/Day 2/dailychallenge/node_modules/cors/lib/index.js 	new file:   Week 10/Day 2/dailychallenge/node_modules/cors/package.json 	new file:   Week 10/Day 2/dailychallenge/node_modules/object-assign/index.js 	new file:   Week 10/Day 2/dailychallenge/node_modules/object-assign/license 	new file:   Week 10/Day 2/dailychallenge/node_modules/object-assign/package.json 	new file:   Week 10/Day 2/dailychallenge/node_modules/object-assign/readme.md 	modified:   Week 10/Day 2/dailychallenge/package-lock.json 	modified:   Week 10/Day 2/dailychallenge/package.json 	modified:   Week 10/Day 2/dailychallenge/server.js

--- Week 10/Day 2/dailychallenge/client/src/App.js
+++ Week 10/Day 2/dailychallenge/client/src/App.js
@@ -1,25 +1,52 @@
 import "./App.css";
-import React, { useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 
 function App() {
+  const [request, setRequest] = useState("Test");
+  const inputText = useRef();
+  const url = "http://localhost:3001/api/hello";
+
+  useEffect(() => {
+    async function fetchData() {
+
+      try {
+        const response = await fetch(url);
+        const text = await response.text();
+        console.log(text);
+        setRequest(text);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    fetchData();
+  }, []);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const message = inputText.current.value;
+    const options = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ message: `I received your POST request. This is what you sent me "${message}"` }),
+    };
+    const resJson = await fetch('http://localhost:3001/api/world', options)
+    const res = await resJson.json()
+    console.log(res)
+
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        {useEffect(() => {
-          // declare the data fetching function
-          const fetchData = async () => {
-            const response = await fetch("http://localhost:3001/api/hello", {
-              mode: "cors",
-            }); 
-            console.log( response)
-            
-          };
+        {request}
 
-          // call the function
-          fetchData()
-            // make sure to catch any error
-            .catch(console.error);
-        }, [])}
+        <form onSubmit={handleSubmit}>
+          <h3>Post to the server</h3>
+          <input type="text" ref={inputText}></input>
+          <input type="submit" value="Submit"></input>
+        </form>
       </header>
     </div>
   );
--- Week 10/Day 2/dailychallenge/server.js
+++ Week 10/Day 2/dailychallenge/server.js
@@ -1,8 +1,10 @@
 const express = require("express");
+const cors = require('cors')
 const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(cors())
 
 app.listen(3001, () => {
   console.log("Running on port 3001");
@@ -11,3 +13,9 @@ app.listen(3001, () => {
 app.get("/api/hello", (req, res) => {
   res.json("Hello From Express");
 });
+
+app.post('/api/world', (req,res) => {
+  console.log(req.body)
+  res.json(req.body)
+  
+})
